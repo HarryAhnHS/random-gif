c56cbd06a0cc2f13d73c7553f5f8b92ba021e2e3
HarryAhnHS <hongsuhahn@gmail.com>
2024-05-30 15:35:49 +0800

    giphy random endpoint

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ import './style/style.css';
 import Git from './images/github.png';
 
 document.getElementById("github").src = Git; // Fill github logo
+
+const img = document.querySelector('img');
+
+fetch('https://api.giphy.com/v1/gifs/random?api_key=ieAJmBUWM0e91Ng4SxLG3jGYx1SQ2SR7', {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response.data.images.original.url);
+        img.src = response.data.images.original.url;
+    });
