e2c479cf654da594238b58c3395e6ef1f92a51d4
HarryAhnHS <hongsuhahn@gmail.com>
2024-05-31 17:35:06 +0800

    async/await conversion

--- src/index.js
+++ src/index.js
@@ -8,40 +8,33 @@ const search = document.querySelector('#search');
 const searchText = document.querySelector('#search-text');
 const errorText = document.querySelector('.error');
 
-function refreshImage() {
+async function refreshImage() {
     if (searchText.value != "") {
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ieAJmBUWM0e91Ng4SxLG3jGYx1SQ2SR7&s=${searchText.value}`, {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            img.src = response.data.images.original.url;
-        })
-        .catch(function(error) {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ieAJmBUWM0e91Ng4SxLG3jGYx1SQ2SR7&s=${searchText.value}`, {mode: 'cors'});
+            const searchData = await response.json();
+            img.src = searchData.data.images.original.url;
+        }
+        catch (error) {
             errorText.textContent = "Request failed: " + error;
-        });
+        }
     }
 }
 
 const random = document.querySelector('#random');
 
-function randomImage() {
-    fetch(`https://api.giphy.com/v1/gifs/random?api_key=S5i4qINf2fE0u8djxMDq6OJaNbFmW1ij`, {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            img.src = response.data.images.original.url;
-        })
-        .catch(function(error) {
-            errorText.textContent = "Request failed: " + error;
-        });
+async function randomImage() {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=S5i4qINf2fE0u8djxMDq6OJaNbFmW1ij`, {mode: 'cors'});
+    const randomData = await response.json();
+    img.src = randomData.data.images.original.url;
 }
 
 random.onclick = function(e) {
     searchText.value = "";
     e.preventDefault();
-    randomImage();
+    randomImage().catch((error) => {
+        errorText.textContent = "Request failed: " + error;
+    });
 }
 
 search.onclick = function(e) {
