b0235cf766779097b8ddc704fc95cc3b1e70041a
HarryAhnHS <hongsuhahn@gmail.com>
2024-05-31 15:41:25 +0800

    search and random feat

--- src/index.js
+++ src/index.js
@@ -4,12 +4,49 @@ import Git from './images/github.png';
 document.getElementById("github").src = Git; // Fill github logo
 
 const img = document.querySelector('img');
+const search = document.querySelector('#search');
+const searchText = document.querySelector('#search-text');
+const errorText = document.querySelector('.error');
 
-fetch('https://api.giphy.com/v1/gifs/random?api_key=ieAJmBUWM0e91Ng4SxLG3jGYx1SQ2SR7', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response.data.images.original.url);
-        img.src = response.data.images.original.url;
-    });
+function refreshImage() {
+    if (searchText.value != "") {
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ieAJmBUWM0e91Ng4SxLG3jGYx1SQ2SR7&s=${searchText.value}`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+        })
+        .catch(function(error) {
+            errorText.textContent = "Request failed: " + error;
+        });
+    }
+}
+
+const random = document.querySelector('#random');
+
+function randomImage() {
+    fetch(`https://api.giphy.com/v1/gifs/random?api_key=S5i4qINf2fE0u8djxMDq6OJaNbFmW1ij`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            img.src = response.data.images.original.url;
+        })
+        .catch(function(error) {
+            errorText.textContent = "Request failed: " + error;
+        });
+}
+
+random.onclick = function(e) {
+    searchText.value = "";
+    e.preventDefault();
+    randomImage();
+}
+
+search.onclick = function(e) {
+    e.preventDefault();
+    refreshImage();
+};
+
+
